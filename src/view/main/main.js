2cd14510c9b520f86ff9290d06465d0b7ce185ac
Astha Pandey <92038030+pandeyastha@users.noreply.github.com>
2023-04-22 14:04:11 +0530

    upd comm

--- src/view/main/main.js
+++ src/view/main/main.js
@@ -6,20 +6,20 @@ import { Post } from './post';
 export const Main =() => {
     const [postsList ,setPostsList] = useState(null);
     const postsRef =collection(db,"posts");
-    
+                                                                        //All post on home
     const getPosts= async () => {
-        const data =await getDocs(postsRef);
+        const data =await getDocs(postsRef);                    // getall documents of "post" collection in DB
         setPostsList(
             data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
           );
     };
-                                                            //useEffect renders once here
+                                                            //useEffect renders once here every refresh
     useEffect(() => {
     getPosts();
         }, []);
-
-    return <div >{postsList?.map((post) => 
-        (<Post post={post} /> 
+                                                                                // calls the post component for rendering
+    return <div >{postsList?.map((post) =>                                                     
+        (<Post post={post} />  
         ))}</div>;
 
 
